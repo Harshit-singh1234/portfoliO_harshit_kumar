0256481a35b961163a792342000f98b2707e69e5
Harshit kumar <82650683+Harshit-singh1234@users.noreply.github.com>
2023-04-21 23:23:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Competative coder", "Developer", "Problem solver", "Designer", "Freelancer"],
+        strings: ["competitive coder", "Developer", "Problem solver", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Competative coder", "Developer", "Problem solver ", "Designer", "Freelancer"],
+        strings: ["competitive coder", "Developer", "Problem solver ", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
